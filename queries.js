f019b386ab4d9ac225f8164bbbc554af3c3607a9
Alison Sipos <alison.sipos@icloud.com>
2020-09-18 14:28:18 -0600

    Configured database connection. Established connection.

---
+++ queries.js
@@ -0,0 +1,14 @@
+const mysql = require("mysql");
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "LoveBud",
+  database: "shoppingcart"
+});
+
+connection.connect();
+
+// export functions
+module.exports = {
+
+}
