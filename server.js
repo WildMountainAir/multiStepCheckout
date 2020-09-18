00891e4f4e037ac71e1d596fd56cca6fd2635140
Alison Sipos <alison.sipos@icloud.com>
2020-09-18 13:48:52 -0600

    Rendered home page, set server to run on port.

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const PORT = 3000;
+const queries = require("./queries");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
@@ -9,3 +11,12 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
+// render home page
+app.post("/", (req, res) => {
+    res.send('index');
+  })
+
+// set server to run on port
+app.listen(PORT, () => {
+    console.log(`🛥  Server is running and listening on port ${PORT} 🛥`);
+  });
