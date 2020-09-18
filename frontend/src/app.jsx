0c3ee9eebb7619bef13863598c329943e582928b
Alison Sipos <alison.sipos@icloud.com>
2020-09-18 14:29:08 -0600

    Converted file to JSX.

---
+++ frontend/src/app.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    this.state = {
+
+    }
+    // handlers that need to be bound
+  } 
+
+  render () {
+      return (
+        <div className="app">
+
+        </div>
+      );
+  }
+}
+
+export default App;
